291508cbbe09b2231ac6682353bf39ef07b050fc
Rodrigo Ballestero <rballestero@oneloop.com.ar>
2020-06-30 02:56:03 -0300

    Add permission for user

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,13 @@ if [ -z "${AUTHORIZED_KEYS}" ]; then
 fi
 
 echo "Populating /root/.ssh/authorized_keys with the value from AUTHORIZED_KEYS env variable ..."
-mkdir -p /home/user/.ssh/
+mkdir /home/user/.ssh/
 echo "${AUTHORIZED_KEYS}" > /home/user/.ssh/authorized_keys
+
+chown -R user:group /home/user
+chmod 700 /home/user/.ssh
+chmod 600 /home/user/.ssh/authorized_keys
+
 
 # Execute the CMD from the Dockerfile:
 exec "$@"
